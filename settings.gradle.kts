8225018e8c8dd142f5f60951eeed3de8856aa5cc
michael <michael.lisc@gmail.com>
2024-11-09 12:26:57 +0800

    提交init文件

---
+++ settings.gradle.kts
@@ -0,0 +1,34 @@
+pluginManagement {
+    repositories {
+        maven( url="https://maven.aliyun.com/repository/google"){
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        maven( url="https://maven.aliyun.com/repository/central")
+        maven( url="https://maven.aliyun.com/repository/gradle-plugin")
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven(url ="https://maven.aliyun.com/repository/google")
+        maven( url="https://maven.aliyun.com/repository/central")
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MyLibrary"
+include(":app")
