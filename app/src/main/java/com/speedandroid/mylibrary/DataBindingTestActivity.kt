009618a1ef827c8d778a84a23c88e51f2635de55
lsc <sun141421@126.com>
2024-11-22 11:10:21 +0800

    降级Gradle

--- app/src/main/java/com/speedandroid/mylibrary/DataBindingTestActivity.kt
+++ app/src/main/java/com/speedandroid/mylibrary/DataBindingTestActivity.kt
@@ -2,7 +2,7 @@ package com.speedandroid.mylibrary
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
+//import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -11,13 +11,13 @@ import com.speedandroid.mylibrary.databinding.ActivityDataBindingTestBinding
 
 class DataBindingTestActivity : AppCompatActivity() {
 
-    private val vm by viewModels<DataBindingViewModel>()
+//    private val vm by viewModels<DataBindingViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val databind = DataBindingUtil.setContentView<ActivityDataBindingTestBinding>(this, R.layout.activity_data_binding_test)
-        databind.vm = vm
+//        val databind = DataBindingUtil.setContentView<ActivityDataBindingTestBinding>(this, R.layout.activity_data_binding_test)
+//        databind.vm = vm
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
