973abd7dc0eda6d712765bc1d0184aaf6d5ef79d
michael <michael.lisc@gmail.com>
2024-12-10 23:41:36 +0800

    实现StatusBarStubView

--- speedCodeLibrary/src/main/java/com/speedandroid/speedcodelibrary/widget/StatusBarStubView.kt
+++ speedCodeLibrary/src/main/java/com/speedandroid/speedcodelibrary/widget/StatusBarStubView.kt
@@ -3,15 +3,28 @@ package com.speedandroid.speedcodelibrary.widget
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
-import com.speedandroid.speedcodelibrary.StatusBarUtils
+import android.view.WindowInsets
+import androidx.core.view.WindowInsetsCompat
 
 /**StatusBar的高度的view*/
 class StatusBarStubView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+    private var statusBarHeight: Int = 0
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val statusBarHeight = StatusBarUtils.getStatusBarHeight(context)
-        setMeasuredDimension(MeasureSpec.getSize(widthMeasureSpec), statusBarHeight)
+        if (statusBarHeight != 0) {
+            setMeasuredDimension(MeasureSpec.getSize(widthMeasureSpec), statusBarHeight)
+        } else {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        }
     }
+
+    override fun onApplyWindowInsets(insets: WindowInsets): WindowInsets {
+        val compat = WindowInsetsCompat.toWindowInsetsCompat(insets, this)
+
+        statusBarHeight = compat.getInsets(WindowInsetsCompat.Type.systemBars()).top
+        return super.onApplyWindowInsets(insets)
+    }
+
 }
